dbef3894242adea5d62d3b3eaea565abbfab0afd
Karan <karanm6505@gmail.com>
2024-11-23 19:54:44 +0530

    xyz

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'http://68.233.118.120:5001/api',
   headers: {
     'Content-Type': 'application/json',
   },
