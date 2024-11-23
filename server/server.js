cda1adf48741092959f810f803181b36dd9db587
Karan <karanm6505@gmail.com>
2024-11-23 18:28:44 +0530

    relapi

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ const path = require('path');
 
 const { fileURLToPath } = require('url');
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
+//  const __filename = fileURLToPath(import.meta.url);
+//  const __dirname = path.dirname(__filename);
+//Hi
 // Middleware
 app.use(cors());
 app.use(express.json());
